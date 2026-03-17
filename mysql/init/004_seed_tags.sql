f9e24b722f89db56efaf6c84e771ffad8259f625
mohammad09010 <almmohammad291@gmail.com>
2026-03-17 08:30:59 +0545

    Seed more item-tag mappings in 004_seed_tags.sql
    Added additional mappings for item tags.

--- mysql/init/004_seed_tags.sql
+++ mysql/init/004_seed_tags.sql
@@ -6,13 +6,12 @@ INSERT IGNORE INTO tags (name) VALUES
 ('Textbook'),
 ('Beginner');
 
--- Map tags to your existing seed items (items 1..4 from 002_seed.sql)
--- 1 The Hobbit -> Fantasy
--- 2 1984 -> Sci-Fi (or Textbook, your choice)
--- 3 Kind of Blue -> Jazz
--- 4 Abbey Road -> Rock
 INSERT IGNORE INTO item_tags (item_id, tag_id) VALUES
 (1, 1),
 (2, 2),
 (3, 4),
-(4, 3);
+(4, 3),
+(5, 6),
+(6, 3),
+(7, 5),
+(8, 4);
