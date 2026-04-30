44b4dd316b323f5558284deebdbfb1ff003316db
Betab <almmohammad291@gmail.com>
2026-04-30 13:19:09 +0545

    Delete mysql/init directory

--- mysql/init/003_tags.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO tags (name, description, cover_image_path) VALUES
-('Fantasy', 'Dive into worlds of magic, mythical creatures, and epic quests.', '/public/images/categories/fantasy-placeholder.png'),
-('Classics', 'Timeless literature and items that have stood the test of time.', '/public/images/categories/classics-placeholder.png'),
-('Rock', 'Vintage vinyl, electric gear, and everything that rocks.', '/public/images/categories/rock-placeholder.png'),
-('Jazz', 'Smooth sounds, brass instruments, and soulful collections.', '/public/images/categories/jazz-placeholder.png'),
-('Sci-Fi', 'The future is here. Tech, gadgets, and otherworldly stories.', '/public/images/categories/sci-fi-placeholder.png'),
-('Mystery', 'Hidden gems, cryptic tales, and secrets waiting to be found.', '/public/images/categories/mystery-placeholder.png');
